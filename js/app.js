0bbf4774b67bd4294f6fc8f71b9957f526a2d446
Matthew Timmons <Matthewbt@yahoo.com>
2018-05-11 22:16:52 -0500

    Added reaction images and color highlighting text

--- js/app.js
+++ js/app.js
@@ -5,14 +5,24 @@ var numberOfButtons = document.getElementsByClassName('button').length;
 var button = document.getElementsByClassName('button');
 var endMessage = "End of game. Return to beginning?";
 
+function rollDie() {
+	console.log( Math.floor(Math.random * 6) + 1);
+}
+
 function generateParagraphTag(words) {
 	return `<p class="button">${words}</p>`;
 }
 
+function generateImage(photo) {
+	return `<img src="${photo}" height="200" width="200">`;
+}
+
 // Function for loading NPC dialogue and player dialogue options
-function nextLine(input, input2) {
+function nextLine(input, input2, input3) {
 	var html = '';
+	html += generateImage(input3);
 	for (i=0; i < input2.length; i++) {
+	
 		html += generateParagraphTag(input2[i]);
 	}
 
@@ -22,33 +32,33 @@ function nextLine(input, input2) {
 }
 
 // Function to input dialogue
-function proceed(input1, input2, j) {
-	document.getElementsByClassName('button')[j].addEventListener('click', () => {nextLine(input1, input2)});
+function proceed(input1, input2, j, input3) {
+	document.getElementsByClassName('button')[j].addEventListener('click', () => {nextLine(input1, input2, input3)});
 }
 
 function runDialogue() {
 	for (var j=0; j < numberOfButtons; j++) {
 		if (button[j].innerHTML === "I've lived here all my life.") {
 			buttons = ['Mr. Bowler is a good friend of mine!', 'Who?'];
-			proceed('Oh really? Then you must know Mr. Bowler.', buttons, j);
+			proceed('Oh really? Then you must know Mr. Bowler.', buttons, j, 'jpg/1.jpg');
 		} else if (button[j].innerHTML === "I came here from Newton.") {
 			buttons = ['I haven\'t heard about any trouble.', 'Did I say Newton? I\'m actually from Springville'];
-			proceed('Newton, eh? I heard there\'s trouble brewing down there.', buttons, j);
+			proceed('Newton, eh? I heard there\'s trouble brewing down there.', buttons, j, 'jpg/Newton.png');
 		} else if (button[j].innerHTML === "Mr. Bowler is a good friend of mine!") {
 			buttons = [endMessage];
-			proceed('You liar! There ain\'t no Mr. Bowler, I made him up!', buttons, j);
+			proceed('You liar! There ain\'t no Mr. Bowler, I made him up!', buttons, j, 'jpg/Liar.jpg');
 		} else if (button[j].innerHTML === "Who?") {
 			buttons = [endMessage];
-			proceed('Don\'t you worry about it. Say, do you have something to eat? I\'m starving.', buttons, j);
+			proceed('Don\'t you worry about it. Say, do you have something to eat? I\'m starving.', buttons, j, 'jpg/Mr. Bowler.jpg');
 		} else if (button[j].innerHTML === 'I haven\'t heard about any trouble.') {
 			buttons = [endMessage];
-			proceed('Don\'t you worry about it. Say, do you have something to eat? I\'m starving.', buttons, j);
+			proceed('Don\'t you worry about it. Say, do you have something to eat? I\'m starving.', buttons, j, 'jpg/Mr. Bowler.jpg');
 		} else if (button[j].innerHTML === 'Did I say Newton? I\'m actually from Springville') {
 			buttons = ['Mr. Bowler is a good friend of mine!', 'Who?'];
-			proceed('Oh really? Then you must know Mr. Bowler.', buttons, j);
+			proceed('Oh really? Then you must know Mr. Bowler.', buttons, j, 'jpg/1.jpg');
 		} else if (button[j].innerHTML === endMessage) {
 			buttons = ['I\'ve lived here all my life.', 'I came here from Newton.'];
-			proceed('You don\'t look like you\'re from around here.', buttons, j);
+			proceed('You don\'t look like you\'re from around here.', buttons, j, "jpg/Suspicious.jpg");
 			document.getElementsByClassName('button').classList.add('endstate');
 		} else {
 			break;
